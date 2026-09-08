010b21f0f4cd95544576af51b8e66e676a51a4a3
4D-12\koyo <a>
2026-09-08 13:41:53 +0900

    enumでゲーム進行を作成 繊維条件や各画面の動きを作成

--- 0908/0908.cpp
+++ 0908/0908.cpp
@@ -1,6 +1,55 @@
 ﻿#include <iostream>
+#include "windows.h"
+
+enum GameProgression
+{
+	Startup,
+	Title,
+	MainMenu,
+	InGame,
+	Result,
+};
 
 int main()
 {
+	GameProgression game_progression = Startup;
 
+	switch(game_progression)
+	{
+		case Startup:
+			std::cout << "Startup" << std::endl;
+			if (GetAsyncKeyState(VK_RETURN))
+			{
+				game_progression = Title;
+				break;
+			}
+		case Title:
+			std::cout << "Title" << std::endl;
+			if (GetAsyncKeyState(VK_RETURN))
+			{
+				game_progression = MainMenu;
+				break;
+			}
+		case MainMenu:
+			std::cout << "MainMenu" << std::endl;
+			if (GetAsyncKeyState(VK_RETURN))
+			{
+				game_progression = InGame;
+				break;
+			}
+		case InGame:
+			std::cout << "InGame" << std::endl;
+			if (GetAsyncKeyState(VK_RETURN))
+			{
+				game_progression = Result;
+				break;
+			}
+		case Result:
+			std::cout << "Result" << std::endl;
+			if (GetAsyncKeyState(VK_RETURN))
+			{
+				game_progression = Startup;
+				break;
+			}
+	}
 }
